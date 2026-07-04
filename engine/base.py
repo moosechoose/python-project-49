4f6a65b90d67016bb942aa62c3bc584d53d69a43
Карина Лосева <los.karina.kon@gmail.com>
2026-07-04 11:55:05 +0200

    логика раундов выведена в отдельный модуль

---
+++ engine/base.py
@@ -0,0 +1,11 @@
+import prompt
+import random
+
+from brain_games.cli import welcome_user
+
+max_rounds = 3
+
+def run_game(game):
+    name = welcome_user()
+    for _ in range(max_rounds):
+        game()
